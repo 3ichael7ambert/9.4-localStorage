b02adf1625a4d781c06348dc855214098e5f33a0
3ichael7ambert <mlambertbusiness@gmail.com>
2023-08-14 17:39:54 -0600

    Update todo.js
    remove from local storage

--- todo.js
+++ todo.js
@@ -1,61 +1,66 @@
-// When the DOM is fully loaded
+// Once the DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
   // Get form and list elements
   const toDoForm = document.getElementById("toDoFormCSS");
   const toDoList = document.getElementById("toDoListCSS");
-
+  
   // Retrieve from localStorage
   const savedToDos = JSON.parse(localStorage.getItem("toDos")) || [];
 
-  // Create To-Do list items from storage
-  for (let i = 0; i < savedToDos.length; i++) {
-    let newToDo = document.createElement("li");
-    newToDo.innerHTML = savedToDos[i].taskBox;
-    let removeButton = document.createElement("button");
-    removeButton.innerText = "X";
-    
-    // Check if the To-Do item is completed and checked off
-    newToDo.isCompleted = savedToDos[i].isCompleted ? true : false;
-    
-    // If completed, decorate appropriately
-    if (newToDo.isCompleted) {
-      newToDo.style.textDecoration = "line-through";
-      newToDo.style.color = "grey";
-      newToDo.classList = "checkOff";
-    }
-    
-    // Append the newToDo to the list
-    toDoList.appendChild(newToDo);
+  // Function to update local storage and refresh the list
+  function updateLocalStorageAndRefresh() {
+    localStorage.setItem("toDos", JSON.stringify(savedToDos));
+    refreshToDoList();
+  }
+
+  // Function to refresh the To-Do list
+  function refreshToDoList() {
+    toDoList.innerHTML = ""; // Clear the current list
+    savedToDos.forEach(todo => {
+      const newToDo = document.createElement("li");
+      newToDo.textContent = todo.task;
+      
+      if (todo.isCompleted) {
+        newToDo.classList.add("checkedOff");
+      }
+      
+      const removeButton = document.createElement("button");
+      removeButton.innerText = "X";
+      removeButton.addEventListener("click", function() {
+        const taskIndex = savedToDos.findIndex(item => item.task === todo.task);
+        if (taskIndex !== -1) {
+          savedToDos.splice(taskIndex, 1);
+          updateLocalStorageAndRefresh();
+        }
+      });
+
+      newToDo.appendChild(removeButton);
+      toDoList.appendChild(newToDo);
+    });
   }
 
   // Listen for form submission
   toDoForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    let removeButton = document.createElement("button");
-    removeButton.innerText = "X";
-    let newToDo = document.createElement("li");
-    newToDo.innerHTML = document.getElementById("taskBox").value;
-    newToDo.isCompleted = false;
-
-    toDoList.appendChild(newToDo);
-    newToDo.appendChild(removeButton);
-    toDoForm.reset();
-
-    // Save to-do to localStorage
-    savedToDos.push({ taskBox: newToDo.innerHTML, isCompleted: false });
-    localStorage.setItem("toDos", JSON.stringify(savedToDos));
+    const taskText = document.getElementById("taskBox").value;
+    if (taskText.trim() !== "") {
+      savedToDos.push({ task: taskText, isCompleted: false });
+      updateLocalStorageAndRefresh();
+      toDoForm.reset();
+    }
   });
 
   // Listen for click events in the list
   toDoList.addEventListener("click", function(e) {
-    const targetTagToLowerCase = e.target.tagName.toLowerCase();
-    if (targetTagToLowerCase === "li") {
-      e.target.style.textDecoration = "line-through";
-      e.target.style.color = "grey";
-      e.target.classList.toggle("checkedOff");
-      e.target.isCompleted = true;
-    } else if (targetTagToLowerCase === "button") {
-      e.target.parentNode.remove();
+    const target = e.target;
+    const listItem = target.closest("li");
+    if (listItem) {
+      const taskText = listItem.textContent;
+      const task = savedToDos.find(item => item.task === taskText);
+      if (target.tagName.toLowerCase() === "li") {
+        task.isCompleted = !task.isCompleted;
+        updateLocalStorageAndRefresh();
+      }
     }
   });
 
@@ -65,6 +70,10 @@ document.addEventListener("DOMContentLoaded", function() {
   // Listen for click on the Clear To-Do button
   clearToDo.addEventListener("click", function() {
     localStorage.clear();
-    location.reload();
+    savedToDos.length = 0;
+    refreshToDoList();
   });
-}); // End DOM loaded
+
+  // Initial list refresh
+  refreshToDoList();
+});
